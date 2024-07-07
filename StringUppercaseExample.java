fb935d7f85204c0740a55f34fec14698d81704f8
developersmita767 <156692415+developersmita767@users.noreply.github.com>
2024-07-07 18:23:48 +0530

    Add files via upload

---
+++ StringUppercaseExample.java
@@ -0,0 +1,10 @@
+public class StringUppercaseExample {
+    public static void main(String[] args)
+    {
+        String str1 = "micky";
+        String str2 = str1.toUpperCase();
+
+        System.out.println(str2);
+    }
+    
+}
